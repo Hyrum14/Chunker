3c4de237df873bf33a086e483ccb813ab70dce0e
Hyrum Reynolds <hyrumreynolds@Hyrums-MacBook-Air.local>
2026-02-27 10:07:02 -0700

    [1059] specified utf-8 encoding

--- src/main/java/app/model/parsing/FileDocumentProvider.java
+++ src/main/java/app/model/parsing/FileDocumentProvider.java
@@ -4,63 +4,53 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class FileDocumentProvider implements DocumentParser.DocumentProvider
-{
+public class FileDocumentProvider implements DocumentParser.DocumentProvider {
     private BufferedReader reader;
     private int nextChar;
 
-    public FileDocumentProvider(String filename) throws IOException
-    {
-        this.reader = new BufferedReader(new FileReader(filename));
+    public FileDocumentProvider(String filename) throws IOException {
+        // always read files as UTF-8 to avoid platform-default charset differences
+        this.reader = new BufferedReader(new java.io.InputStreamReader(
+                new java.io.FileInputStream(filename), java.nio.charset.StandardCharsets.UTF_8));
         this.nextChar = reader.read();
         printChar();
     }
 
-    public FileDocumentProvider(BufferedReader reader) throws IOException
-    {
+    public FileDocumentProvider(BufferedReader reader) throws IOException {
         this.reader = reader;
         this.nextChar = reader.read();
         printChar();
     }
 
     @Override
-    public boolean end()
-    {
+    public boolean end() {
         return nextChar == -1;
     }
 
     @Override
-    public int peek()
-    {
-        if (end())
-        {
+    public int peek() {
+        if (end()) {
             throw new IndexOutOfBoundsException("No more characters");
         }
         return nextChar;
     }
 
     @Override
-    public void advance() throws IndexOutOfBoundsException
-    {
-        try
-        {
-            if (end())
-            {
+    public void advance() throws IndexOutOfBoundsException {
+        try {
+            if (end()) {
                 throw new IndexOutOfBoundsException("No more characters");
             }
             nextChar = reader.read();
             printChar();
-        } catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
             throw new IndexOutOfBoundsException(e.getMessage());
         }
     }
 
-    private void printChar()
-    {
-//        if (!end())
-//            System.out.printf("%c [UTF+%04x]%n", nextChar, nextChar);
+    private void printChar() {
+        // if (!end())
+        // System.out.printf("%c [UTF+%04x]%n", nextChar, nextChar);
     }
 }
-
